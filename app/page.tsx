3429300f35fc5047c7d84cde10602b787c18aa1a
Banele Blessed <100396901+Sqwizoor@users.noreply.github.com>
2025-01-08 10:55:28 +0000

    helllo hear

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,33 @@ import FeaturedSection from '@/components/FeaturedSection'
 import NewsSection from '@/components/NewsSection'
 import FAQSection from '@/components/FAQSection'
 import FloatingIcons from './about/FixedButtons'
+import BlogList from './components/BlogList'
+import { client } from '@/lib/sanity.client'
 
 
-export default function Home() {
+
+async function getPosts() {
+  const posts = await client.fetch(`*[_type == "post"] | order(publishedAt desc) {
+    _id,
+    title,
+    slug,
+    mainImage,
+    publishedAt,
+    author->{name}
+  }`)
+  return posts
+}
+
+export default async function Home() {
+
+  const posts = await getPosts()
+
   return (
     <main>
       <HeroSection />
       <WelcomeSection />
       <FeaturedSection />
-      <NewsSection />
+       <BlogList posts={posts} />
       <FloatingIcons/>
       <FAQSection />
     </main>
